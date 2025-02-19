69ad2045aadaf2eb777fee0f1034b590b30da4d4
Daniel M Sussman <dmsussman@gmail.com>
2025-02-18 20:21:48 -0500

    optional title string

--- lua/motleyLatex.lua
+++ lua/motleyLatex.lua
@@ -16,7 +16,7 @@ M.config = {
 -- merge the config, and define a user command
 M.setup = function(opts)
     M.config = vim.tbl_deep_extend("force", M.config, opts or {})
-    module.config = M.config
+    module.config = M.config --- do this here *and* later so that one can overwrite the title each time
 
     vim.api.nvim_create_user_command('MotleyLatex',
         function(opts)
@@ -38,17 +38,37 @@ M.setup = function(opts)
                 end_line = vim.api.nvim_buf_get_mark(0, ">")[1]
             end
 
-            local latex_code = module.generateLatexCodeblock(start_line, end_line)
+            -- argument parsing
+            local args = {}
+            start,b,c,quotedString = string.find(opts.args, "([\"'])(.-)%1")
+            if start then
+                arg = string.sub(opts.args,1,start-2)
+                table.insert(args, arg)-- Match anything that's not a quote, and quoted strings.
+                table.insert(args,quotedString)
+            else
+                if opts.args and opts.args ~= "" then
+                    table.insert(args,opts.args)
+                end
+            end
 
             -- Determine output file name
             local output_file
-            if opts.args and opts.args ~= "" then
+            if args[1] then
                 -- Use the provided argument as the output file basename
-                output_file = opts.args .. '.tex'
+                output_file = args[1] .. '.tex'
             else
                 -- Use the current file's name as the default
                 output_file = vim.fn.expand('%:p:r') .. '.tex'
             end
+
+            if args[2] then 
+                module.config.tcolorbox_opts.title = args[2]
+            else
+                module.config.tcolorbox_opts.title = ""
+            end
+
+            local latex_code = module.generateLatexCodeblock(start_line, end_line)
+
 
             -- Write the LaTeX code to the output file
             local file = io.open(output_file, "w")
@@ -60,7 +80,7 @@ M.setup = function(opts)
                 print("Error: Could not write to file " .. output_file)
             end
         end,
-        { range = true, nargs = '?', complete = 'file', desc = 'save a tcolorbox corresponding to the current color scheme. Acts on whole buffer or visual selection. optional argument specifies the filename that will be saved' })
+        { range = true, nargs = '*', complete = 'file', desc = 'save a tcolorbox corresponding to the current color scheme. Acts on whole buffer or visual selection. optional argument specifies the filename that will be saved' })
 
     -- vim.api.nvim_create_user_command("MotleyLatexTesting",
     --     function()
